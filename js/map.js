1279abafd0f0140a146909886336b73f5c0cd010
André Carvalho <afacarvalho@yahoo.com.br>
2020-10-20 15:02:17 -0300

    Using max date to first call

--- js/map.js
+++ js/map.js
@@ -6,19 +6,28 @@ var api_key = "afdb1e8a9c8142739553e3942283d6c8";
 
 $( document ).ready(function() {
     loadMosaicsFromApi();
-    loadMap('08','2020');
 });
 
 function loadMosaicsFromApi(){
     let url="https://api.planet.com/basemaps/v1/mosaics?api_key="+api_key+"&_page_size=1000";
+    
     $.ajax({url: url, success: function(result){
+        let maxDate=new Date('2000-01-01');
+        // first, read all periods from API
         result.mosaics.forEach(mosaic => {
             let dt=mosaic.first_acquired.split('T')[0];
+            maxDate=(maxDate<new Date(dt+'T12:00:00'))?(new Date(dt+'T12:00:00')):(maxDate);
             let y=dt.split('-')[0];
             let m=dt.split('-')[1];
             let option='<option value="'+m+'-'+y+'">'+m+'/'+y+'</option>';
             $('#mosaics').append(option);
         });
+        maxDate=maxDate.toISOString().split('T')[0];
+        let maxMonth=maxDate.split('-')[1];
+        let maxYear=maxDate.split('-')[0];
+        $('#mosaics').val(maxMonth+'-'+maxYear);
+        // so, call map with max month and year
+        loadMap(maxMonth, maxYear);
     }});
 };
 
@@ -30,7 +39,6 @@ function changeMonth(ev) {
 
 function reloadMap(m,y){
     if(tms_example) tms_example.removeFrom(map);
-
     let url="https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_"+y+"_"+m+"_mosaic/gmap/{z}/{x}/{y}.png?api_key="+api_key;
 
     tms_example = L.tileLayer(url, {
