04b5c9967b05ca045bc590709e59e7e8e0042d40
André Carvalho <afacarvalho@yahoo.com.br>
2020-10-20 14:27:12 -0300

    Load all periods from api

--- js/map.js
+++ js/map.js
@@ -2,22 +2,36 @@ var map;
 var tms_example;
 var latitude = -55.685277;
 var longitude = -10.3;
+var api_key = "afdb1e8a9c8142739553e3942283d6c8";
 
 $( document ).ready(function() {
+    loadMosaicsFromApi();
     loadMap('08','2020');
 });
+
+function loadMosaicsFromApi(){
+    let url="https://api.planet.com/basemaps/v1/mosaics?api_key="+api_key+"&_page_size=1000";
+    $.ajax({url: url, success: function(result){
+        result.mosaics.forEach(mosaic => {
+            let dt=mosaic.first_acquired.split('T')[0];
+            let y=dt.split('-')[0];
+            let m=dt.split('-')[1];
+            let option='<option value="'+m+'-'+y+'">'+m+'/'+y+'</option>';
+            $('#mosaics').append(option);
+        });
+    }});
+};
 
 function changeMonth(ev) {
     let m=ev.value.split('-')[0];
     let y=ev.value.split('-')[1];
-    console.log(m+'-'+y);
     reloadMap(m,y);
 };
 
 function reloadMap(m,y){
     if(tms_example) tms_example.removeFrom(map);
 
-    let url="https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_"+y+"_"+m+"_mosaic/gmap/{z}/{x}/{y}.png?api_key=afdb1e8a9c8142739553e3942283d6c8";
+    let url="https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_"+y+"_"+m+"_mosaic/gmap/{z}/{x}/{y}.png?api_key="+api_key;
 
     tms_example = L.tileLayer(url, {
         tms: false
