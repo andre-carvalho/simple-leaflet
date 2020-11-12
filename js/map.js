530f9ed4d8be90d68a038c3134005019c8195e4f
André Carvalho <afacarvalho@yahoo.com.br>
2020-11-12 16:47:45 -0300

    Improve the load of the layers because the API has changed.

--- js/map.js
+++ js/map.js
@@ -13,40 +13,55 @@ function loadMosaicsFromApi(){
     
     $.ajax({url: url, success: function(result){
         let maxDate=new Date('2000-01-01');
+        let layers=[],times=[];
         // first, read all periods from API
         result.mosaics.forEach(mosaic => {
-            let dt=mosaic.first_acquired.split('T')[0];
-            maxDate=(maxDate<new Date(dt+'T12:00:00'))?(new Date(dt+'T12:00:00')):(maxDate);
-            let y=dt.split('-')[0];
-            let m=dt.split('-')[1];
-            let option='<option value="'+m+'-'+y+'">'+m+'/'+y+'</option>';
+            layers.push(mosaic.name);
+            let startdt=mosaic.first_acquired.split('T')[0];
+            let enddt=mosaic.last_acquired.split('T')[0];
+            times.push({
+                startdt:startdt,
+                enddt:enddt,
+                lname:mosaic.name
+            });
+            maxDate=(maxDate<new Date(startdt+'T12:00:00'))?(new Date(startdt+'T12:00:00')):(maxDate);
+        });
+        let selectedLayer='';
+        maxDate=maxDate.toISOString().split('T')[0];
+        let maxdt=maxDate.split('-')[0]+'-'+maxDate.split('-')[1];
+        times.sort(function(a, b){return new Date(a.startdt) - new Date(b.startdt)});
+        times.forEach(l=>{
+            let selected='',type='Visual';
+            if(l.lname.includes(maxdt)) {
+                selectedLayer=l.lname;
+                selected='selected';
+            }
+            if(l.lname.includes('normalized_analytic')){
+                type='Normalizada';
+            }
+            let option='<option value="'+l.lname+'" '+selected+'>'+type+' de '+l.startdt.replaceAll('-','/')+' até '+l.enddt.replaceAll('-','/')+'</option>';
             $('#mosaics').append(option);
         });
-        maxDate=maxDate.toISOString().split('T')[0];
-        let maxMonth=maxDate.split('-')[1];
-        let maxYear=maxDate.split('-')[0];
-        $('#mosaics').val(maxMonth+'-'+maxYear);
-        // so, call map with max month and year
-        loadMap(maxMonth, maxYear);
+        $('#mosaics').val(selectedLayer);
+        // so, call map with the layer name
+        loadMap(selectedLayer);
     }});
 };
 
 function changeMonth(ev) {
-    let m=ev.value.split('-')[0];
-    let y=ev.value.split('-')[1];
-    reloadMap(m,y);
+    reloadMap(ev.value);
 };
 
-function reloadMap(m,y){
+function reloadMap(selectLayer){
     if(tms_example) tms_example.removeFrom(map);
-    let url="https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_"+y+"_"+m+"_mosaic/gmap/{z}/{x}/{y}.png?api_key="+api_key;
+    let url="https://tiles.planet.com/basemaps/v1/planet-tiles/"+selectLayer+"/gmap/{z}/{x}/{y}.png?api_key="+api_key;
 
     tms_example = L.tileLayer(url, {
         tms: false
     }).addTo(map);
 };
 
-function loadMap(m,y) {
+function loadMap(selectLayer) {
     map = L.map('map', {
         scrollWheelZoom: true,
         fullscreenControl: {
@@ -59,7 +74,7 @@ function loadMap(m,y) {
     //     [-6.4917667, 39.0945000]
     // ]);
 
-    reloadMap(m,y);
+    reloadMap(selectLayer);
 }
 
 
